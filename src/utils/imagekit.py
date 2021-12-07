8c0ae789b659f9ef4ee6e8837b609e688f197b5e
Vioshim <63890837+Vioshim@users.noreply.github.com>
2021-12-07 17:33:03 -0500

    Fixes

--- src/utils/imagekit.py
+++ src/utils/imagekit.py
@@ -10,9 +10,7 @@
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
+#  limitations under the License.#  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
 #
@@ -23,6 +21,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 from base64 import b64encode
 from typing import Iterable
 
