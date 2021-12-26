a5b0389416f5505f70165ccd37812d85d6977c47
Vioshim <Krangelj@unicartagena.edu.co>
2021-12-26 14:20:17 -0500

    fix

--- src/cogs/submission/cog.py
+++ src/cogs/submission/cog.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from asyncio import TimeoutError
 from contextlib import suppress
 from datetime import timedelta
 from pathlib import Path
