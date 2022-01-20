b8111a65e50864fdeae8bf4af4a0b41cec3e83e7
Vioshim <Krangelj@unicartagena.edu.co>
2022-01-20 11:54:10 -0500

    fix

--- src/structures/bot.py
+++ src/structures/bot.py
@@ -104,32 +104,7 @@ class CustomBot(Bot):
         self.msg_cache: set[int] = set()
         self.dagpi = DagpiClient(getenv("DAGPI_TOKEN"))
 
-    async def process_commands(self, message: Message) -> None:
-        """|coro|
-
-        This function processes the commands that have been registered
-        to the bot and other groups. Without this coroutine, none of the
-        commands will be triggered.
-
-        By default, this coroutine is called inside the :func:`.on_message`
-        event. If you choose to override the :func:`.on_message` event, then
-        you should invoke this coroutine as well.
-
-        This is built using other low level tools, and is equivalent to a
-        call to :meth:`~.Bot.get_context` followed by a call to :meth:`~.Bot.invoke`.
-
-        This also checks if the message's author is a bot and doesn't
-        call :meth:`~.Bot.get_context` or :meth:`~.Bot.invoke` if so.
-
-        Parameters
-        -----------
-        message: :class:`discord.Message`
-            The message to process commands for.
-        """
-
-        if message.author:
-            return
-
+    async def on_message(self, message):
         if message.content:
             for url in REGEX_URL.findall(message.content):
                 with suppress(ClientConnectorError):
@@ -150,9 +125,7 @@ class CustomBot(Bot):
                             else:
                                 await message.reply("That's a Nitro Scam")
                             return
-
-        ctx = await self.get_context(message)
-        await self.invoke(ctx)
+        await self.process_commands(message)
 
     def msg_cache_add(self, message: Union[Message, PartialMessage, int], /):
         """Method to add a message to the message cache
