f534bc6fcdcddedc326b379e69cd7e4320ef2e06
Vioshim <Krangelj@unicartagena.edu.co>
2022-02-05 19:51:14 -0500

    fix

--- src/pagination/complex.py
+++ src/pagination/complex.py
@@ -89,7 +89,7 @@ class Complex(Simple):
         self._keep_working = keep_working
         self._choices: Optional[set[_T]] = None
         if keep_working:
-            self._max_values = max(max_values, len(list(self.values)))
+            self._max_values = max(max_values, len(list(values)))
         else:
             self._max_values = max_values
         if isinstance(emoji_parser, str):
