07bea6a0ac42baacccb2629eecdef4a89fdff311
Vioshim <Krangelj@unicartagena.edu.co>
2022-04-09 02:13:29 -0500

    fix

--- src/cogs/roles/area_selection.py
+++ src/cogs/roles/area_selection.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from itertools import groupby
+from threading import Thread
 
 from discord import (
     CategoryChannel,
@@ -61,8 +62,16 @@ class AreaSelection(View):
             if ch and cat == ch.category:
                 return cat
 
+        def foo2(oc: Character):
+            ch = guild.get_channel_or_thread(oc.location)
+            if isinstance(ch, Thread):
+                ch = ch.parent
+            return ch
+
         entries = groupby(cog.ocs.values(), key=foo)
+        entries2 = groupby(cog.ocs.values(), key=foo2)
         self.entries = {str(k.id): set(v) for k, v in entries if k}
+        self.entries2 = {str(k.id): set(v) for k, v in entries2 if k}
         self.total = sum(len(item) for item in self.entries.values())
 
         def handle(item: TextChannel) -> str:
@@ -92,7 +101,7 @@ class AreaSelection(View):
             channel.name,
         )
 
-        ocs = self.entries.get(sct.values[0], set())
+        ocs = self.entries2.get(sct.values[0], set())
         view = CharactersView(
             target=ctx,
             member=ctx.user,
