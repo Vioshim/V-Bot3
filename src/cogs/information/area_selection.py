0c7d86806917602e12d61819cee5beec9099b193
Vioshim <Krangelj@unicartagena.edu.co>
2022-07-23 02:38:17 -0500

    fix

--- src/cogs/information/area_selection.py
+++ src/cogs/information/area_selection.py
@@ -74,8 +74,9 @@ class AreaSelection(Complex[TextChannel]):
     @select(row=1, placeholder="Select a location to check", custom_id="selector")
     async def select_choice(self, interaction: Interaction, sct: Select) -> None:
         try:
+            resp: InteractionResponse = interaction.response
+            await resp.defer(ephemeral=True, thinking=True)
             channel: TextChannel = self.current_choice
-            self.bot.logger.info("%s is reading Channel Information of %s", str(interaction.user), channel.name)
             ocs = self.entries.get(channel.id, set())
             view = CharactersView(target=interaction, member=interaction.user, ocs=ocs, keep_working=True)
             embed = view.embed
