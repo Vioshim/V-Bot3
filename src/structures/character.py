64cf5dfad9f33936ec212f43b950a9d358430eeb
Vioshim <Krangelj@unicartagena.edu.co>
2022-08-04 11:27:51 -0500

    Fix

--- src/structures/character.py
+++ src/structures/character.py
@@ -645,7 +645,7 @@ class Character:
                 THREAD, MOVESET, TYPES, ABILITIES, SPECIES, HIDDEN_POWER
             )
             VALUES (
-                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18,
+                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18
             )
             ON CONFLICT (ID) DO UPDATE
             SET
