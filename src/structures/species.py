f0feb5cb2a54697a4c3c22e15c978a80bc5fa4aa
Vioshim <Krangelj@unicartagena.edu.co>
2022-08-24 17:31:45 -0500

    Fix

--- src/structures/species.py
+++ src/structures/species.py
@@ -988,7 +988,7 @@ class SpeciesDecoder(JSONDecoder):
         Any
             Output
         """
-        if all(i in dct for i in Species.__slots__):
+        if all(i in Species.__slots__ for i in dct):
             match dct.pop("kind", ""):
                 case "Legendary":
                     return Legendary(**dct)
