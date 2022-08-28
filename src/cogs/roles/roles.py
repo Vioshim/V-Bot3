7c1682fdd7876291113b362a8d9d57937d7f6819
Vioshim <Krangelj@unicartagena.edu.co>
2022-08-28 09:08:35 -0500

    Fix

--- src/cogs/roles/roles.py
+++ src/cogs/roles/roles.py
@@ -163,7 +163,7 @@ class AFKSchedule:
         return "\n".join(
             f"• {o[0].strftime('%I:00 %p')} - {o[-1].strftime('%I:59 %p')}"
             for k, v in groupby(map(time, range(24)), key=lambda x: x.hour in self.hours)
-            if k and (o := [datetime.combine(reference, x, tz) for x in v])
+            if k and (o := [datetime.combine(reference, x).astimezone(tz) for x in v])
         )
 
     @classmethod
