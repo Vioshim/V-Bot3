b1ca3477db2cced2f073feef55136b20eca1404f
Vioshim <Krangelj@unicartagena.edu.co>
2022-09-03 21:46:41 -0500

    Fix

--- src/cogs/inviter/__init__.py
+++ src/cogs/inviter/__init__.py
@@ -39,6 +39,7 @@ from src.cogs.inviter.classifier import InviterView
 from src.pagination.complex import Complex
 from src.structures.bot import CustomBot
 from src.utils.etc import WHITE_BAR
+from src.utils.imagekit import ImageKit
 from src.utils.matches import INVITE
 
 __all__ = ("Inviter", "setup")
@@ -178,9 +179,10 @@ class Inviter(commands.Cog):
 
         embed.description = INVITE.sub(invite.url, embed.description)
         if not embed.image or embed.image.url == WHITE_BAR:
-            file = await guild.banner.to_file()
-            embed.set_image(url=f"attachment://{file.filename}")
-            attachments.append(file)
+            image = ImageKit(guild.banner.url, height=540, width=960)
+            if file := await self.bot.get_file(image.url):
+                embed.set_image(url=f"attachment://{file.filename}")
+                attachments.append(file)
 
         msgs = [x for x in self.view.messages if x.id != reference.id]
         msgs.append(reference)
@@ -258,9 +260,10 @@ class Inviter(commands.Cog):
             generator.set_image(url=f"attachment://{file.filename}")
             files.append(file)
         elif invite_guild.banner:
-            file = await invite_guild.banner.to_file()
-            generator.set_image(url=f"attachment://{file.filename}")
-            files.append(file)
+            image = ImageKit(guild.banner.url, height=540, width=960)
+            if file := await self.bot.get_file(image.url):
+                generator.set_image(url=f"attachment://{file.filename}")
+                files.append(file)
 
         link_view = View()
         link_view.add_item(Button(label="Click Here to Join", url=invite.url))
