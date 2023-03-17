713fd917824980f848863ea855ef2b80796fce16
Vioshim <Krangelj@unicartagena.edu.co>
2023-03-17 05:34:15 -0500

    Fix

--- src/structures/character.py
+++ src/structures/character.py
@@ -653,7 +653,7 @@ class Character:
                 )
                 or (isinstance(self.species, Fusion) and self.species.possible_types != {self.types})
             ):
-                name1 += " " + "".join(str(x.emoji) for x in self.types)
+                name1 += "\n" + "".join(str(x.emoji) for x in self.types)
             c_embed.add_field(name=name1, value=name2)
 
         for index, ability in enumerate(sorted(self.abilities, key=lambda x: x.name), start=1):
