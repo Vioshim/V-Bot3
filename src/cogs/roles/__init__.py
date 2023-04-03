00b8471ccd0b0f7e077e5fcaaf57966fc001cd1f
Kevin Rangel <krangelj@unicartagena.edu.co>
2023-04-03 13:03:09 -0500

    Fix

--- src/cogs/roles/__init__.py
+++ src/cogs/roles/__init__.py
@@ -151,7 +151,7 @@ class Roles(commands.Cog):
 
         no_ping_role = get(msg.guild.roles, id=1092498088347844649)
         if no_ping_role and (no_ping_users := ", ".join(str(x) for x in users if x != no_ping_role)):
-            await msg.reply(embed="https://media.tenor.com/kJhT6VC2tzEAAAAC/pings-off-reply-pings-off.gif")
+            await msg.reply("https://media.tenor.com/kJhT6VC2tzEAAAAC/pings-off-reply-pings-off.gif")
             await msg.author.timeout(timedelta(seconds=5), reason=f"Pinged {no_ping_users}")
 
         offline_users = [x.id for x in users if str(x.status) == "offline"]
