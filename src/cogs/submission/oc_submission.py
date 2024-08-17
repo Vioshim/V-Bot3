29b292da053ada39ecd407c6ab3e86c85ef8f114
Vioshim <Krangelj@unicartagena.edu.co>
2024-08-17 03:50:37 -0500

    refactor: Update AgeField validation condition in oc_submission.py

--- src/cogs/submission/oc_submission.py
+++ src/cogs/submission/oc_submission.py
@@ -455,7 +455,7 @@ class AgeField(TemplateField, required=True):
 
     @classmethod
     def evaluate(cls, oc: Character) -> Optional[str]:
-        if not oc.age or oc.age == AgeGroup.Timeless:
+        if oc.age in (None, AgeGroup.Timeless, AgeGroup.Unknown):
             return "Not a valid age group."
 
     @classmethod
