813fceb35e9e08b6367a3fd39a57bf0e39ff0195
Vioshim <Krangelj@unicartagena.edu.co>
2024-12-14 15:04:40 -0500

    refactor: Implement role check in checker function for command access control

--- src/cogs/ai/__init__.py
+++ src/cogs/ai/__init__.py
@@ -101,6 +101,17 @@ FREE_RES_OPUS = [
 ]
 
 
+def checker(ctx: commands.Context[CustomBot]):
+
+    if guild := get(ctx.author.mutual_guilds, id=1196879060173852702):
+        member = guild.get_member(ctx.author.id)
+
+        if member and get(member.roles, id=1307054749727850526):
+            return True
+
+    raise commands.UserInputError("You must have the RP role to use this command")
+
+
 class AiCog(commands.Cog):
 
     def __init__(self, bot: CustomBot):
@@ -119,7 +130,7 @@ class AiCog(commands.Cog):
         await self.client.close()
 
     @commands.max_concurrency(1, wait=False)
-    @commands.check(lambda ctx: get(ctx.author.mutual_guilds, id=1196879060173852702) is not None)
+    @commands.check(checker)
     @commands.hybrid_command(nsfw=True)
     @app_commands.allowed_installs(users=True, guilds=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
@@ -214,7 +225,7 @@ class AiCog(commands.Cog):
                     ucPreset=UndesiredPreset.HEAVY,
                 )
 
-            if (result := payload.calculate_cost(is_opus=True)) and ctx.author.id != 339957281921695745:
+            if (result := payload.calculate_cost(is_opus=True)) and not ctx.bot.is_owner(ctx.author):
                 raise commands.UserInputError(f"Estimated cost: {result} credits")
 
             msg = await ctx.reply(
